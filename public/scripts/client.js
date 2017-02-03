2c8c62b688eb2d3b568b726a2f3eed901073bac9
charles <charles@charless-MacBook-Pro.local>
2017-02-03 11:13:29 -0600

    final code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,71 +3,24 @@ var app = angular.module('giphyApp', []);
 app.controller('giphyController', function(giphyService) {
   console.log('Giphy Controller loaded!')
 
-//var url = 'http://api.giphy.com';
-
-var ctrl = this;
-
-ctrl.randomGiphy=function(){
-giphyService.getRandomGiphy().then(function(image_url){
-console.log('inside success from randomGiphy function call');
-ctrl.imageUrl=image_url;
-});
-}
-
-ctrl.searchGiphy=function(){
-giphyService.getSearchGiphy(ctrl.search).then(function(giphyList){
-  console.log('inside success from searchGiphy function call',giphyList);
-  ctrl.giphyList=giphyList;
-});
-}
-// $http.get(url + '/v1/gifs/random', {
-//     params: {
-//       api_key: 'dc6zaTOxFJmzC'
-//     }
-// }).then(function(response) {
-//   console.log('grabbing random giphy: ', response);
-// }).catch(function(err){
-//   console.log('error getting data', err);
-// })
 
 
-// ctrl.randomGiphy = function() {
-//
-//   ctrl.imageUrl=
-//   console.log('Random button was clicked!')
-//   $http.get(url + '/v1/gifs/random', {
-//       params: {
-//         api_key: 'dc6zaTOxFJmzC'
-//       }
-//   }).then(function(response) {
-//     console.log('grabbing random giphy: ', response.data.data.image_url);
-//     ctrl.imageUrl = response.data.data.image_url;
-//   }).catch(function(err){
-//     console.log('error getting data', err);
-//   })
-// }
+  var ctrl = this;
+  // function handles button click for random Giphy search
+  ctrl.randomGiphy=function(){
+    giphyService.getRandomGiphy().then(function(image_url){
+      console.log('inside success from randomGiphy function call');
+      ctrl.imageUrl=image_url;
+    });
+  }
 
-
-
-// ctrl.searchGiphy = function() {
-//   console.log('search field was used!');
-//   console.log(ctrl.search);
-//   $http.get(url + '/v1/gifs/search', {
-//       params: {
-//         api_key: 'dc6zaTOxFJmzC',
-//         q: ctrl.search
-//       }
-//   }).then(function(response) {
-//     ctrl.giphyList = response.data.data;
-//     response.data.data.forEach(function(obj){
-//       console.log(obj.images.original.url);
-//     })
-//     console.log('grabbing random giphy: ', response.data.data);
-//     // ctrl.imageUrl = response.data.data.image_url;
-//   }).catch(function(err){
-//     console.log('error getting data', err);
-//   })
-// }
+ // function handles user search for Giphys
+  ctrl.searchGiphy=function(){
+    giphyService.getSearchGiphy(ctrl.search).then(function(giphyList){
+      console.log('inside success from searchGiphy function call',giphyList);
+      ctrl.giphyList=giphyList;
+    });
+  }
 
 
 });
--- public/scripts/giphy-service.js
+++ public/scripts/giphy-service.js
@@ -3,45 +3,45 @@ app.service('giphyService',function($http){
   var url = 'http://api.giphy.com';
 
 
-this.getRandomGiphy=function(){
+  this.getRandomGiphy=function(){
 
-  console.log('Inside giphy-service randomGiphy function!');
- return   $http.get(url + '/v1/gifs/random', {
+    console.log('Inside giphy-service randomGiphy function!');
+    return   $http.get(url + '/v1/gifs/random', {
       params: {
         api_key: 'dc6zaTOxFJmzC'
       }
-  }).then(function(response) {
-    console.log('grabbing random giphy: ', response.data.data.image_url);
-    return response.data.data.image_url;
-  }).catch(function(err){
-    console.log('error getting data', err);
-  })
+    }).then(function(response) {
+      console.log('grabbing random giphy: ', response.data.data.image_url);
+      return response.data.data.image_url;
+    }).catch(function(err){
+      console.log('error getting data', err);
+    })
 
-}// end of getRandomGiphy function
+  }// end of getRandomGiphy function
 
 
 
-this.getSearchGiphy = function(search) {
+  this.getSearchGiphy = function(search) {
 
-  console.log('search field entry:: ',search);
-  return $http.get(url + '/v1/gifs/search', {
+    console.log('search field entry:: ',search);
+    return $http.get(url + '/v1/gifs/search', {
       params: {
         api_key: 'dc6zaTOxFJmzC',
         q: search
       }
-  }).then(function(response) {
+    }).then(function(response) {
 
-    response.data.data.forEach(function(obj){
-      //console.log(obj.images.original.url);
-      console.log(obj);
-    })
-    console.log('grabbing random giphy: ', response.data.data);
-    // ctrl.imageUrl = response.data.data.image_url;
+      response.data.data.forEach(function(obj){
+        //console.log(obj.images.original.url);
+        console.log(obj);
+      })
+      console.log('grabbing random giphy: ', response.data.data);
+      // ctrl.imageUrl = response.data.data.image_url;
       return response.data.data;
-  }).catch(function(err){
-    console.log('error getting data', err);
-  })
-} // end of searchGiphy function
+    }).catch(function(err){
+      console.log('error getting data', err);
+    })
+  } // end of searchGiphy function
 
 
 });
